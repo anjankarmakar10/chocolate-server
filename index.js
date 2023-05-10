5f95b344fa3d3ec3719cad94a21f9c795f9a0337
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-05-10 09:46:29 +0600

    implememted database and also get, post request

--- index.js
+++ index.js
@@ -7,6 +7,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://chocolate_admin:bCIUI5XtwhRp7xJj@cluster0.d2cwisz.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const chocolateCollaction = client
+      .db("chocolateDB")
+      .collection("chocolateCollaction");
+
+    app.get("/chocolates", async (req, res) => {
+      const cursor = chocolateCollaction.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/chocolates", async (req, res) => {
+      const chocolate = req.body;
+      const result = await chocolateCollaction.insertOne(chocolate);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+run();
+
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
